7c579d97e86fdef56df8e05d566192fe44be051c
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-05-23 14:29:37 +0800

    add embeds

--- bot.js
+++ bot.js
@@ -2,24 +2,52 @@ require('dotenv').config(); // Add this line at the top of your bot.js file
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const Discord = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 
-const bot = new Discord.Client();
+const bot = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
 const app = express();
 const port = 3000; // Choose the port number you want to use
 
 app.use(bodyParser.json());
 
 app.post('/notifications', (req, res) => {
-  const notification = req.body;
+  const { title, content } = req.body;
 
   const channel = bot.channels.cache.get(process.env.DISCORD_CHANNEL_ID); // Use the environment variable for the channel ID
 
-  // Format the notification payload
-  const notificationMessage = JSON.stringify(notification, null, 2);
+  const status = content.status.toUpperCase();
 
-  // Send the notification as a message
-  channel.send(notificationMessage);
+  // Create a MessageEmbed for the rich message
+  const embed = new EmbedBuilder()
+    .setTitle(title)
+    .setDescription(content.info)
+    .addFields({ name: 'Status', value: `**${status}**`, inline: true })
+    .setTimestamp();
+
+  // Customize the embed based on the status
+  switch (content.status) {
+    case 'passed':
+      embed.setColor('#36a64f'); // Green
+      break;
+    case 'fail':
+      embed.setColor('#ff0000'); // Red
+      break;
+    case 'done':
+      embed.setColor('#f1c40f'); // Yellow
+      break;
+    case 'deployed':
+      embed.setColor('#3498db'); // Blue
+      break;
+    default:
+      embed.setColor('#000000'); // Black
+      break;
+  }
+
+  // Send the embed as a message
+  channel.send({ embeds: [embed] });
 
   res.status(200).send('Notification received and sent to Discord.');
 });
